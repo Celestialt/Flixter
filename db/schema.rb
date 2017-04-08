432ae50380f72d4bc08ce0670e183756b5695f7f
Kirstan Fitzptrick <you@example.com>
2017-04-08 05:38:23 +0000

    Generated alter_lessons_add_row_order migrations file; adjusted the file; included RankedModel gem in app/models/section.rb file; added rank(:row_order) to app/views/courses/show.html.erb file and to app/views/instructor/courses/show.html.erb file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407020914) do
+ActiveRecord::Schema.define(version: 20170408051429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20170407020914) do
     t.integer  "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "row_order"
+    t.index ["row_order"], name: "index_sections_on_row_order", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
